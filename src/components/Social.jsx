990da3a3e10a63c8cc4d270441d9cda970efb446
martei95 <51247353+marteiduel@users.noreply.github.com>
2022-01-24 23:05:17 -0700

    socialmediacomment

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import {
-  FiFacebook,
-  FiTwitter,
-  FiInstagram,
-  FiDribbble,
+  // FiFacebook,
+  // FiTwitter,
+  // FiInstagram,
+  // FiDribbble,
   FiLinkedin,
 } from "react-icons/fi";
 
 const SocialShare = [
-  {
-    iconName: <FiFacebook />,
-    link: "https://www.facebook.com/",
-  },
-  { iconName: <FiTwitter />, link: "https://twitter.com/" },
-  {
-    iconName: <FiInstagram />,
-    link: "https://www.instagram.com/",
-  },
-  { iconName: <FiDribbble />, link: "https://dribbble.com/" },
+  // {
+  //   iconName: <FiFacebook />,
+  //   link: "https://www.facebook.com/",
+  // },
+  // { iconName: <FiTwitter />, link: "https://twitter.com/" },
+  // {
+  //   iconName: <FiInstagram />,
+  //   link: "https://www.instagram.com/",
+  // },
+  // { iconName: <FiDribbble />, link: "https://dribbble.com/" },
   {
     iconName: <FiLinkedin />,
-    link: "https://www.linkedin.com/feed/",
+    link: "https://www.linkedin.com/in/marteichagoya/",
   },
 ];
 const Social = () => {
