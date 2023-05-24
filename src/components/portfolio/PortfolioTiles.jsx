894477d46dcb06555bad849c89d58d9f7bedd23a
Duel <51247353+marteiduel@users.noreply.github.com>
2023-05-24 10:46:21 -0600

    spa mar azul

--- src/components/portfolio/PortfolioTiles.jsx
+++ src/components/portfolio/PortfolioTiles.jsx
@@ -4,36 +4,36 @@ import PortfolioTile from "./PortfolioTile";
 const PortfolioTiles = () => {
   return (
     <div>
-      <div className='portfoliotiles'>
+      <div className="portfoliotiles">
         <PortfolioTile
-          title='Elunex'
-          imageSource={"../../../img/portfolio/s0De8M89S3.png"}
-          website='https://elunex.com/'
+          title="Mar Spa Azul"
+          imageSource={"../../../img/portfolio/vJaUY9wiAb.png"}
+          website="https://spamarazul.com/"
         />
         <PortfolioTile
-          title='Hungry Budget'
+          title="Hungry Budget"
           imageSource={"../../../img/portfolio/lTbQeBxSAT.png"}
-          website='https://hungrybudget.com/'
+          website="https://hungrybudget.com/"
         />
         <PortfolioTile
-          title='Alta Design & Construction'
+          title="Alta Design & Construction"
           imageSource={"../../../img/portfolio/OGvKg54DFE.png"}
-          website='http://glassrepair.marteiportfolio.site'
+          website="https://adc.homes/"
         />
         <PortfolioTile
-          title='Master Stylist'
+          title="Master Stylist"
           imageSource={"../../../img/portfolio/MasterStylist.jpg"}
-          website='https://www.masterstylistutah.com/'
+          website="https://www.masterstylistutah.com/"
         />
         <PortfolioTile
-          title='DevCampFries'
+          title="DevCampFries"
           imageSource={"../../../img/portfolio/2.jpg"}
-          website='https://devcamp-fries-martei.netlify.app'
+          website="https://devcamp-fries-martei.netlify.app"
         />
         <PortfolioTile
-          title='Delipan'
+          title="Delipan"
           imageSource={"../../../img/portfolio/1.jpg"}
-          website='https://delipanutah.com/'
+          website="https://delipanutah.com/"
         />
       </div>
     </div>
